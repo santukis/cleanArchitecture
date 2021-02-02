e36d54afd1005503b53f6c8fc23e16d69df3766a
David <santamadavid@gmail.com>
2021-02-02 20:47:45 +0100

    create AsyncUseCaseExecutor.kt as the first Executor.kt

--- app/src/main/java/com/santukis/cleanarchitecture/core/model/Executor.kt
+++ app/src/main/java/com/santukis/cleanarchitecture/core/model/Executor.kt
@@ -5,7 +5,7 @@ interface Executor {
     fun <Params, Result> execute(useCase: UseCase<Params, Result>,
                                  params: Params,
                                  onResult: (Response<Result>) -> Unit = {})
-    
+
     suspend fun <Params, Result> execute(useCase: UseCase<Params, Result>,
                                          params: Params): Response<Result>
 }
---
+++ app/src/main/java/com/santukis/cleanarchitecture/core/model/executors/AsyncUseCaseExecutor.kt
@@ -0,0 +1,38 @@
+package com.santukis.cleanarchitecture.core.model.executors
+
+import com.santukis.cleanarchitecture.core.model.Executor
+import com.santukis.cleanarchitecture.core.model.Response
+import com.santukis.cleanarchitecture.core.model.UseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+class AsyncUseCaseExecutor: Executor, CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    override fun <Params, Result> execute(useCase: UseCase<Params, Result>, params: Params, onResult: (Response<Result>) -> Unit) {
+        launch(coroutineContext) {
+            useCase.run(params)
+                .flowOn(Dispatchers.IO)
+                .catch { exception -> onResult(Response.Error(exception)) }
+                .collect { response -> onResult(response) }
+        }
+    }
+
+    override suspend fun <Params, Result> execute(useCase: UseCase<Params, Result>, params: Params): Response<Result> {
+        return withContext(coroutineContext) {
+            useCase.run(params)
+                .flowOn(Dispatchers.IO)
+                .catch { exception -> Response.Error<Result>(exception) }
+                .single()
+        }
+    }
+}
