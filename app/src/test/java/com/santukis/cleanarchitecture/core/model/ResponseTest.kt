ef229208dcc90be533ff9d6869f2b922607c0976
David <santamadavid@gmail.com>
2021-02-02 20:34:58 +0100

    added some test to Response model

---
+++ app/src/test/java/com/santukis/cleanarchitecture/core/model/ResponseTest.kt
@@ -0,0 +1,37 @@
+package com.santukis.cleanarchitecture.core.model
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ResponseTest {
+
+    @Test
+    fun whenResponseIsSuccessDataShouldBeAvailable() {
+        when(val expectedResponse = Response.Success("Expected Response")) {
+            is Response.Success -> assertEquals("Expected Response", expectedResponse.data)
+            is Response.Error<*> -> fail()
+        }
+    }
+
+    @Test
+    fun whenResponseIsErrorErrorFieldShouldBeAvailable() {
+        when(val expectedResponse = Response.Error<String>(Exception("Expected Error"))) {
+            is Response.Success<*> -> fail()
+            is Response.Error<String> -> assertEquals("Expected Error", expectedResponse.error.message)
+        }
+    }
+
+    @Test
+    fun isSuccessfulShouldReturnTrueWhenResponseIsSuccessful() {
+        val response = Response.Success("Expected Response")
+
+        assertTrue(response.isSuccessful())
+    }
+
+    @Test
+    fun isSuccessfulShouldReturnFalseWhenResponseIsError() {
+        val response = Response.Error<String>(Exception("Expected Response"))
+
+        assertFalse(response.isSuccessful())
+    }
+}
