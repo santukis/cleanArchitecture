fbb8da887ce9e10154af75aa477aef77222be30c
David <santamadavid@gmail.com>
2021-02-28 20:02:30 +0100

    created MetArtwork.kt entity and museums DataSources

--- app/src/test/java/com/santukis/cleanarchitecture/artwork/data/remote/ArtworkDtoParsing.kt
+++ app/src/test/java/com/santukis/cleanarchitecture/artwork/data/remote/ArtworkDtoParsing.kt
@@ -19,7 +19,7 @@ class ArtworkDtoParsing {
 
     @Test
     fun artWorkResponseAdapterShouldReturnExpectedItemsWhenResponseContainsMultipleItems() {
-        val adapter = moshi.adapter(ArtworkResponse::class.java).lenient()
+        val adapter = moshi.adapter(RijksMuseumResponse::class.java).lenient()
 
         val items = adapter.fromJson(ArtworkDataProvider.artWorkResponseMultipleItems)?.items
         assertEquals(2, items?.size)
@@ -31,32 +31,32 @@ class ArtworkDtoParsing {
         assertEquals("", sampleItem?.description)
         assertEquals("Rembrandt van Rijn", sampleItem?.author)
         assertEquals("https://lh3.googleusercontent.com/AyiKhdEWJ7XmtPXQbRg_kWqKn6mCV07bsuUB01hJHjVVP-ZQFmzjTWt7JIWiQFZbb9l5tKFhVOspmco4lMwqwWImfgg=s0", sampleItem?.image?.url)
-        assertEquals(DatingDto.EMPTY, sampleItem?.dating)
-        assertEquals(emptyList<DimensionDto>(), sampleItem?.dimensions)
-        assertEquals(emptyList<ColorDto>(), sampleItem?.colors)
+        assertEquals(RijksMuseumDating.EMPTY, sampleItem?.dating)
+        assertEquals(emptyList<RijksMuseumDimension>(), sampleItem?.dimensions)
+        assertEquals(emptyList<RijksMuseumColor>(), sampleItem?.colors)
     }
 
     @Test
     fun artWorkResponseAdapterShouldReturnExpectedItemWhenResponseContainsOneItem() {
-        val adapter = moshi.adapter(ArtworkResponse::class.java).lenient()
+        val adapter = moshi.adapter(RijksMuseumResponse::class.java).lenient()
         val item =  adapter.fromJson(ArtworkDataProvider.artworkResponseSingleItem)?.item
 
-        assertNotEquals(ArtworkDto.EMPTY, item)
+        assertNotEquals(RijksMuseumArtwork.EMPTY, item)
         assertEquals("SK-C-6", item?.id)
         assertEquals("The Sampling Officials of the Amsterdam Drapers’ Guild, Known as ‘The Syndics’", item?.title)
         assertEquals("Samplers checked the quality of dyed cloth. Here Rembrandt shows them at work, distracted for a moment and looking up. One syndic is about to sit, or stand, so not all the heads are at the same level. A clever trick which, with the confident brushwork and subtle use of light, make this one of the liveliest group portraits of the 17th century. Originally painted for the sampling hall (Staalhof), in 1771 it was acquired by Amsterdam’s town hall.", item?.description)
         assertEquals("Rembrandt van Rijn", item?.author)
         assertEquals("https://lh3.googleusercontent.com/gShVRyvLLbwVB8jeIPghCXgr96wxTHaM4zqfmxIWRsUpMhMn38PwuUU13o1mXQzLMt5HFqX761u8Tgo4L_JG1XLATvw=s0", item?.image?.url)
-        assertEquals(DatingDto(1662), item?.dating)
+        assertEquals(RijksMuseumDating(1662), item?.dating)
         assertEquals(2, item?.dimensions?.size)
-        assertEquals(DimensionDto(type = "height", unit = "cm", value = "191.5"), item?.dimensions?.getOrNull(0))
+        assertEquals(RijksMuseumDimension(type = "height", unit = "cm", value = "191.5"), item?.dimensions?.getOrNull(0))
         assertEquals(6, item?.colors?.size)
-        assertEquals(ColorDto(color = "#8B7759", normalizedColor = "#E0CC91"), item?.colors?.getOrNull(0))
+        assertEquals(RijksMuseumColor(color = "#8B7759", normalizedColor = "#E0CC91"), item?.colors?.getOrNull(0))
     }
 
     @Test
     fun artWorkResponseAdapterShouldReturnNullValuesWhenResponseFieldsAreNulls() {
-        val adapter = moshi.adapter(ArtworkResponse::class.java).lenient()
+        val adapter = moshi.adapter(RijksMuseumResponse::class.java).lenient()
         val item =  adapter.fromJson(ArtworkDataProvider.artworkResponseSingleItemWithNulls)?.item
 
         assertEquals("SK-C-6", item?.id)
@@ -66,7 +66,7 @@ class ArtworkDtoParsing {
 
     @Test
     fun toArtWorkShouldReturnDefaultValuesWhenDtoValuesAreNull() {
-        val artworkDto = ArtworkDto(
+        val artworkDto = RijksMuseumArtwork(
             id = null,
             title = null,
             description = null,
