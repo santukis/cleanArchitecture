91b4031c4f7fe8efa0ffa2baf063d906c3832bf0
David <santamadavid@gmail.com>
2021-03-01 16:32:13 +0100

    artworks module:  implemented ChicagoArtworkDataSource.kt

--- app/src/main/java/com/santukis/cleanarchitecture/artwork/data/datasources/ChicagoArtworkDataSource.kt
+++ app/src/main/java/com/santukis/cleanarchitecture/artwork/data/datasources/ChicagoArtworkDataSource.kt
@@ -1,4 +1,33 @@
 package com.santukis.cleanarchitecture.artwork.data.datasources
 
-class ChicagoArtworkDataSource: ArtworkDataSource {
+import com.santukis.cleanarchitecture.BuildConfig
+import com.santukis.cleanarchitecture.artwork.domain.model.Artwork
+import com.santukis.cleanarchitecture.artwork.domain.model.Collection
+import com.santukis.cleanarchitecture.core.data.remote.HttpClient
+import com.santukis.cleanarchitecture.core.domain.model.Response
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class ChicagoArtworkDataSource(private val client: HttpClient = HttpClient(host = BuildConfig.CHICAGO_END_POINT)): ArtworkDataSource {
+
+    companion object {
+        const val MAX_ITEM_SIZE = 50
+    }
+
+    override suspend fun loadArtworks(collection: Collection, lastItem: Int): Flow<Response<List<Artwork>>> =
+        flow {
+            try {
+                val artworks = client.artworkService.loadChicagoArtworks(
+                    page = (lastItem / MAX_ITEM_SIZE) + 1,
+                    size = MAX_ITEM_SIZE
+                ).data
+
+                when {
+                    artworks.isNullOrEmpty() -> super.loadArtworks(collection, lastItem)
+                    else -> emit(Response.Success(artworks.map { it.toArtwork() }))
+                }
+            } catch (exception: Exception) {
+                emit(Response.Error<List<Artwork>>(exception))
+            }
+        }
 }
