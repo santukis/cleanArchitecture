e5b047aeeb325238861538d18e2e55ac6b31befe
David <santamadavid@gmail.com>
2021-03-02 18:26:49 +0100

    added collection parameter to LocalArtworkDataSource.kt loadArtworks query

--- app/src/main/java/com/santukis/cleanarchitecture/artwork/data/local/ArtworkDao.kt
+++ app/src/main/java/com/santukis/cleanarchitecture/artwork/data/local/ArtworkDao.kt
@@ -5,14 +5,15 @@ import androidx.room.Query
 import androidx.room.RawQuery
 import androidx.room.Transaction
 import androidx.sqlite.db.SupportSQLiteQuery
+import com.santukis.cleanarchitecture.artwork.domain.model.Collection
 import com.santukis.cleanarchitecture.core.data.local.BaseDao
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArtworkDao: BaseDao<ArtworkDb> {
 
-    @Query("SELECT * FROM artworks")
-    fun loadArtworks(): Flow<List<ArtworkDb>>
+    @Query("SELECT * FROM artworks WHERE collection = :collection")
+    fun loadArtworks(collection: Collection): Flow<List<ArtworkDb>>
 
     @Query("SELECT * FROM artworks WHERE artworks.id = :artworkId LIMIT 1")
     fun loadArtwork(artworkId: String): ArtworkDetailDb?
