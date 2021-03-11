8edfae54d4b381facd2379bb043fe068539044ce
David <santamadavid@gmail.com>
2021-03-11 18:02:03 +0100

    created PuzzleGameActivity.kt

---
+++ app/src/main/java/com/santukis/cleanarchitecture/game/ui/activities/PuzzleGameActivity.kt
@@ -0,0 +1,22 @@
+package com.santukis.cleanarchitecture.game.ui.activities
+
+import android.util.Size
+import com.santukis.cleanarchitecture.core.domain.model.Response
+import com.santukis.cleanarchitecture.core.ui.activities.BaseActivity
+import com.santukis.cleanarchitecture.databinding.ActivityPuzzleGameBinding
+
+class PuzzleGameActivity: BaseActivity<ActivityPuzzleGameBinding>() {
+
+
+    override fun getViewBinding(): ActivityPuzzleGameBinding = ActivityPuzzleGameBinding.inflate(layoutInflater)
+
+    override fun initializeViewComponents() {
+        gameViewModel?.loadPuzzle()
+
+        gameViewModel?.puzzle?.observe(this) { response ->
+            when(response) {
+                is Response.Success -> binding.container.createPuzzle(response.data.image, Size(8, 7))
+            }
+        }
+    }
+}
