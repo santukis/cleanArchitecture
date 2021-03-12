74fecd742b6bf180389d8d04638dff5da9c7baf4
David <santamadavid@gmail.com>
2021-03-12 07:44:56 +0100

    created basic Game daos

---
+++ app/src/main/java/com/santukis/cleanarchitecture/game/data/local/GameDao.kt
@@ -0,0 +1,7 @@
+package com.santukis.cleanarchitecture.game.data.local
+
+import com.santukis.cleanarchitecture.core.data.local.BaseDao
+
+interface PuzzleDao: BaseDao<PuzzleDb>
+
+interface PieceDao: BaseDao<PieceDao>
